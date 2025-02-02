9dabd5e9565909d601fa00d3e83453c45d4fe0d6
Hussain Ali <iamhussainali2@gmail.com>
2025-02-02 14:35:19 +0530

    added shadcn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,16 @@
 export default function Home() {
   return(
     <>
-    <div>Home</div>
+    <section className="pink_container">
+      <h1 className="heading">Pitch your startup, connect with entrepreneurs</h1>
+      <p className="sub-heading !max-w-3xl">
+        Submit Ideas, Vote on Ideas, and Connect with Entrepreneurs.
+
+      </p>
+      
+    </section>
+      
+  
     
     </>
 
