1d14dfe18394802e98381ae4e3d8e20b8c3b6d2d
Hussain Ali <iamhussainali2@gmail.com>
2025-02-02 20:20:51 +0530

    added hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,66 @@
+import SearchForm from "@/components/SearchForm";
+import { auth } from "@/auth";
+import StartupCard from "@/components/StartupCard";
 
-
-export default function Home() {
-  return(
-    <>
-    <section className="pink_container">
-      <h1 className="heading">Pitch your startup, connect with entrepreneurs</h1>
-      <p className="sub-heading !max-w-3xl">
-        Submit Ideas, Vote on Ideas, and Connect with Entrepreneurs.
-
-      </p>
-      
-    </section>
-      
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   
-    
-    </>
+  const posts = [
+    {
+      _createdAt: new Date().toDateString(),
+      views:55,
+      author:{_id:1, name:"John Doe"},
+      _id:1,
+      description:"This is a description",
+      image:"https://picperf.io/https://laravelnews.s3.amazonaws.com/featured-images/whenLoaded.png",
+      category:"Programming",
+      title:"How programming evolved",
+    },
+  ]
 
-  )
+  const session = await auth();
+
+  console.log(session?.user?.id);
+
+  
+
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="card-grid">
+
+          {
+            posts?.length > 0 ? (
+              posts.map((post:StartupTypeCard) => <StartupCard key={post._id} post={post} />)
+            ):(<p className="no-result">No startups found</p>)
+          }
+
+        </ul>
+
+      
+      </section>
+    </>
+  );
 }
---
+++ components/StartupCard.tsx
@@ -0,0 +1,55 @@
+import { EyeIcon } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { Button } from './ui/button'
+
+const StartupCard = ({post}:{post:StartupTypeCard}) => {
+
+    const {description, image, title, category, author, _id} = post
+  return (
+    <li className='startup-card group'>
+<div className=' flex-between'>
+<p className=' startup_card_date'>{post?._createdAt}</p>
+
+<div className=' flex gap-1.5'>
+    <EyeIcon className='size-6 text-primary'/>
+    <span className=' text-16-medium'>{post?.views}</span>
+</div>
+
+</div>
+
+<div className=' flex-between mt-5 gap-5'>
+<div className=' flex-1'>
+    <Link href={`/user/${post?.author?._id}`}>
+    <p className=' text-16-medium line-clamp-1'>{post?.author?.name}</p>
+    </Link>
+    <Link href={`/startup/${_id}`}>
+    <h3 className=' text-26-semibold line-clamp-1'>
+        {title}
+    </h3>
+    </Link>
+</div>
+<Link href={`/user/${post?.author?._id}`}>
+<Image src={"https://placehold.co/48x48"} alt='placeholder' width={48} height={48} className='rounded-full' />
+</Link>
+</div>
+<Link href={`/startup/${_id}`}>
+<p className='startup-card_desc'>
+    {description}
+</p>
+<img src={image} alt=""/>
+</Link>
+<div className=' flex-between mt-5 gap-3'>
+<Link href={`/?query=${category.toLowerCase()}`}>
+<p className=' text-16-medium'>{category}</p>
+</Link>
+<Button className='startup-card_btn' asChild>
+    <Link href={`/startup/${_id}`}>Details</Link>
+</Button>
+</div>
+    </li>
+  )
+}
+
+export default StartupCard
